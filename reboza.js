42865c41db7ea0e689f3e89231ac08e07b464f35
rebozasambirejo <cah.sambirejo47@gmail.com>
2017-09-20 17:15:49 +0700

    Create reboza.js

---
+++ reboza.js
@@ -0,0 +1,14 @@
+var homePage = window.location.origin,numPosts = 7;
+function downloadJSAtOnload(){var d=document.createElement("script");d.src="https://cdn.rawgit.com/KompiAjaib/kompi-js/master/recent_post_with_thumbnail.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+
+<script src='https://static.adpxl.com/adpxl2.min.js' type='text/javascript'/>
+  var key = &quot;7O4nKVcSuD1YqEAqEQlV13pcnBlYf3YH1UvwAO0B&quot;, pubId=573;
+  try {
+    adpxl.init(key,pubId,window.location.href);
+  } catch (err) {
+  }
+
+
+//LazyLoad Image
+function loadScript(d){var o=document.createElement("script");o.src=d,document.body.appendChild(o)}function downloadJSAtOnload(){loadScript("https://rawgit.com/mastamvan/backup/master/lazyload.js")}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+//]]>
